a5f9aff37a535f0f9ef17e895bf33a53239631e7
bensonfx <fuxin04@gmail.com>
2018-02-23 18:40:25 +0800

    fetch gosu from github

--- gerrit/image/fetch_gerrit.sh
+++ gerrit/image/fetch_gerrit.sh
@@ -9,6 +9,15 @@ GITILES_PLUGIN_VERSION=bazel-master-stable-2.15
 DELPROJ_PLUGIN_VERSION=bazel-stable-2.15
 EVENTSLOG_PLUGIN_VERSION=bazel-master
 GERRIT_OAUTH_URL="https://github.com/davido/gerrit-oauth-provider/releases"
+GOSU_URL="https://github.com/tianon/gosu/releases"
+
+fetch_gosu() {
+    local GOSU_QUERY=$(curl -ksSL ${GOSU_URL} | grep -oE "download/v[0-9.]+/gosu-amd64"| head -n1)
+
+    local GOSU="/usr/bin/gosu"
+    curl -Lo $GOSU "${GOSU_URL}/${GOSU_QUERY}"
+    chmod a+x $GOSU
+}
 
 fetch_plugins() {
     curl -fSsL \
@@ -32,6 +41,7 @@ fetch_plugins() {
 fetch_gerrit_war() {
     curl -fsSL http://repo1.maven.org/maven2/com/google/gerrit/gerrit-war/${GERRIT_VERSION}/gerrit-war-${GERRIT_VERSION}.war \
         -o ${GERRIT_WAR}
+    fetch_gosu
 }
 
 case $1 in
