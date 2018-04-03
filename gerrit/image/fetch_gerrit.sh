8e9c20038383809580b8c8910b01f02490e75f56
bensonfx <fuxin04@gmail.com>
2018-04-03 10:39:46 +0800

    move env setup to fetch_gerrit.sh script

--- gerrit/image/fetch_gerrit.sh
+++ gerrit/image/fetch_gerrit.sh
@@ -2,6 +2,8 @@
 
 set -ex
 DEBIAN_FRONTEND=noninteractive
+GERRIT_WAR_URL="http://repo1.maven.org/maven2/com/google/gerrit/gerrit-war/${GERRIT_VERSION}/gerrit-war-${GERRIT_VERSION}.war"
+
 GERRITFORGE_URL=https://gerrit-ci.gerritforge.com
 GERRITFORGE_ARTIFACT_DIR=lastSuccessfulBuild/artifact/bazel-genfiles/plugins
 IMPORTER_PLUGIN_VERSION=bazel-master
@@ -10,6 +12,25 @@ DELPROJ_PLUGIN_VERSION=bazel-stable-2.15
 EVENTSLOG_PLUGIN_VERSION=bazel-master
 GERRIT_OAUTH_URL="https://github.com/davido/gerrit-oauth-provider/releases"
 GOSU_URL="https://github.com/tianon/gosu/releases"
+
+
+
+clean() {
+    rm -rf /var/lib/apt/lists/* /tmp/*
+}
+
+init_env() {
+    echo " # Preparing ..."
+    sed -i "s@http://archive.ubuntu.com@http://mirrors.aliyun.com@g" /etc/apt/sources.list
+    useradd -Ulms /sbin/nologin ${GERRIT_USER}
+    chmod a+x ${GERRIT_HOME}/*.sh
+    mkdir /entrypoint-init.d
+    chown -R ${GERRIT_USER} ${GERRIT_HOME}
+    apt-get update
+    # apt-get install -yqq --no-install-recommends openjdk-8-jdk curl git openssh-client vim
+    apt-get install -yqq openjdk-8-jdk curl git openssh-client
+    clean
+}
 
 fetch_gosu() {
     local GOSU_QUERY=$(curl -ksSL ${GOSU_URL} | grep -oE "download/v[0-9.]+/gosu-amd64"| head -n1)
@@ -42,8 +63,8 @@ fetch_plugins() {
 }
 
 fetch_gerrit_war() {
-    curl -fsSL http://repo1.maven.org/maven2/com/google/gerrit/gerrit-war/${GERRIT_VERSION}/gerrit-war-${GERRIT_VERSION}.war \
-        -o ${GERRIT_WAR}
+    init_env
+    curl -L ${GERRIT_WAR_URL} -o ${GERRIT_WAR}
     #fetch_gosu
 }
 
