1fd91e88fe2b7ecc16604bbd0df12ba11e9eb3e7
Benson Yan <fuxin04@gmail.com>
2018-08-16 20:59:27 +0800

    add lam php security config

--- gerrit/lam/install_lam.sh
+++ gerrit/lam/install_lam.sh
@@ -7,6 +7,36 @@ LAM_PKG=ldap-account-manager-${VER}.tar.bz2
 LAM_URL=http://prdownloads.sourceforge.net/lam/${LAM_PKG}?download
 LAM_DIR=${DOCKER_LAM_DIR:-"/wwwroot/lam"}
 
+PHP_CONF_DIR=$(php --ini | grep additional | grep -o "/.*/etc/php.*")
+FPM_CONF_DIR="/usr/local/etc/php-fpm.d"
+
+config_php() {
+    #config php session
+    local php_cfg=${PHP_CONF_DIR}/extra.ini
+    local PHP_SESSION_DIR=${WEB_ROOT}/zentaopms/tmp/session
+cat > ${php_cfg} << EOF
+session.save_handler = "files"
+session.save_path    = "${PHP_SESSION_DIR}"
+expose_php = false
+cgi.fix_pathinfo = 0
+date.timezone = Asia/Shanghai
+short_open_tag = On
+upload_max_filesize = 50M
+disable_functions = passthru,exec,system,chroot,chgrp,chown,shell_exec,proc_open,proc_get_status,ini_alter,ini_restore,dl,openlog,syslog,readlink,symlink,popepassthru,stream_socket_server,fsocket,popen
+EOF
+    #run php-fpm as deamon
+    # local php_fpm_docker_cfg=${FPM_CONF_DIR}/zz-docker.conf
+    # sed -i "s#^daemonize = .*#daemonize = yes#g" $php_fpm_docker_cfg
+    # sed -i "s#listen = .*#listen = /dev/shm/php-cgi.sock#g" $php_fpm_docker_cfg
+
+    local php_fpm_www_cfg=${FPM_CONF_DIR}/www.conf
+    sed -i "s#^pm.max_children =.*#pm.max_children = 50#g" $php_fpm_www_cfg
+    sed -i "s#^pm.start_servers =.*# pm.start_servers = 30#g" $php_fpm_www_cfg
+    sed -i "s#^pm.min_spare_servers = .*#pm.min_spare_servers = 20#g" $php_fpm_www_cfg
+    sed -i "s#^pm.max_spare_servers = .*#pm.max_spare_servers = 50#g" $php_fpm_www_cfg
+    sed -i "s#^;pm.max_requests = .*#pm.max_requests = 2048#g" $php_fpm_www_cfg
+}
+
 init_deps() {
     #apt-get install -yqq locales tzdata
     #locale-gen zh_CN.UTF-8
@@ -50,6 +80,7 @@ compile_lam() {
 }
 
 init_deps
+config_php
 download_extract_lam
 compile_lam
 clean
