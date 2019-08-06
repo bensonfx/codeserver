1830a532764331d246bef1b13cd794d380b160aa
Benson Yan <bensonfx@outlook.com>
2019-08-06 23:04:11 +0800

    fix update version bugs

--- nextcloud/aria2/aria2c.sh
+++ nextcloud/aria2/aria2c.sh
@@ -35,24 +35,32 @@ run_aria2() {
 }
 
 prepare() {
-    local aria2_path=/usr/local/www/aria2
+    #local aria2_path=/usr/local/www/aria2
+    aria2_path=/var/www/html/aria
+
     local aria_ng=$(find /root/ -name AriaNg-*.zip)
     [ -d "$aria2_path" ] || install -d $aria2_path $aria2_path/Download
     if [ $(ls -A $aria2_path| wc -w) -lt 1 ];then
         cd $aria2_path
         unzip $aria_ng
-        rm -rf $aria_ng
-        chown -R www-data:root ${aria2_path}
+        #rm -rf $aria_ng
+        #chown -R www-data:root ${aria2_path}
+        chown -R 82:root ${aria2_path}
         chmod -R 775 ${aria2_path}
         cd -
     fi
 
     echo "check version"
     local curr_ver=$(cd $aria2_path;grep -oE "buildVersion:\"v[0-9.]+" js/aria-ng-*.min.js |grep -oE "[0-9.]+")
-    local docker_ver=$(echo AriaNg-*.zip | grep -oE "[0-9.]+[0-9]")
+    local docker_ver=$(find /root -name AriaNg-*.zip | grep -oE "[0-9.]+[0-9]")
+    [ -z "$docker_ver" ] && return
+
     if [ "$curr_ver" != "$docker_ver" ];then
         echo "update ariaNg version from $curr_ver to $docker_ver"
+        cd ${aria2_path}
+        rm -rf ${aria2_path}/*
         unzip -o $aria_ng
+        cd -
     fi
 }
 
